fddbc23b73b0d33113a8409aba69db10b5730e9c
2022BCD0042-Jayanth <duggammagari22bcd42@iiitkottayam.ac.in>
2026-02-23 15:43:33 +0530

    Create train.py
    lab2

---
+++ train.py
@@ -0,0 +1,31 @@
+import os, json, joblib, pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+os.makedirs("output", exist_ok=True)
+
+df = pd.read_csv("dataset/winequality-red.csv", sep=";")
+X = df.drop("quality", axis=1)
+y = df["quality"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("MSE:", mse)
+print("R2 Score:", r2)
+
+joblib.dump(model, "output/model.pkl")
+json.dump(
+    {"experiment": "EXP-08", "model": "RF(70-30)", "mse": mse, "r2_score": r2},
+    open("output/results.json", "w"), indent=4
+)
