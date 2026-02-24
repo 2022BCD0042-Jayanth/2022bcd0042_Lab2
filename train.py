6656b80318a47b86b70e150a239c91210ae95c02
2022BCD0042-Jayanth <duggammagari22bcd42@iiitkottayam.ac.in>
2026-02-24 09:35:35 +0530

    Update train.py
    EXP-4

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import os, json, joblib, pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Lasso
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 os.makedirs("output", exist_ok=True)
@@ -13,9 +15,12 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = Lasso(alpha=0.01)
-model.fit(X_train, y_train)
+model = Pipeline([
+    ("scaler", StandardScaler()),
+    ("lr", LinearRegression())
+])
 
+model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
 mse = mean_squared_error(y_test, y_pred)
@@ -26,6 +31,6 @@ print("R2 Score:", r2)
 
 joblib.dump(model, "output/model.pkl")
 json.dump(
-    {"experiment": "EXP-03", "model": "Lasso(alpha=0.01)", "mse": mse, "r2_score": r2},
+    {"experiment": "EXP-04", "model": "LinearRegression+Scaler", "mse": mse, "r2_score": r2},
     open("output/results.json", "w"), indent=4
 )
