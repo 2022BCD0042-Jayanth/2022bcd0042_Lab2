048e9fa83ca99d24d2813b2d9ae4d6c8e2c4cd0d
2022BCD0042-Jayanth <duggammagari22bcd42@iiitkottayam.ac.in>
2026-02-24 09:41:20 +0530

    Update train.py
    EXP-07

--- train.py
+++ train.py
@@ -6,20 +6,21 @@ from sklearn.metrics import mean_squared_error, r2_score
 os.makedirs("output", exist_ok=True)
 
 df = pd.read_csv("dataset/winequality-red.csv", sep=";")
-X = df.drop("quality", axis=1)
+
+selected_features = [
+    "alcohol", "sulphates", "citric acid", "volatile acidity"
+]
+
+X = df[selected_features]
 y = df["quality"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = RandomForestRegressor(
-    n_estimators=200,
-    max_depth=10,
-    random_state=42
-)
-
+model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
+
 y_pred = model.predict(X_test)
 
 mse = mean_squared_error(y_test, y_pred)
@@ -30,6 +31,6 @@ print("R2 Score:", r2)
 
 joblib.dump(model, "output/model.pkl")
 json.dump(
-    {"experiment": "EXP-06", "model": "RF(200,depth=10)", "mse": mse, "r2_score": r2},
+    {"experiment": "EXP-07", "model": "RF(SelectedFeatures)", "mse": mse, "r2_score": r2},
     open("output/results.json", "w"), indent=4
 )
