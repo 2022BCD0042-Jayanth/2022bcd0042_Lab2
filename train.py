0b35af7237e1687571d60302f90a910ebe980cc4
2022BCD0042-Jayanth <duggammagari22bcd42@iiitkottayam.ac.in>
2026-02-24 09:40:04 +0530

    Update train.py
    EXP-6

--- train.py
+++ train.py
@@ -13,9 +13,13 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = RandomForestRegressor(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+model = RandomForestRegressor(
+    n_estimators=200,
+    max_depth=10,
+    random_state=42
+)
 
+model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
 mse = mean_squared_error(y_test, y_pred)
@@ -26,6 +30,6 @@ print("R2 Score:", r2)
 
 joblib.dump(model, "output/model.pkl")
 json.dump(
-    {"experiment": "EXP-05", "model": "RF(100)", "mse": mse, "r2_score": r2},
+    {"experiment": "EXP-06", "model": "RF(200,depth=10)", "mse": mse, "r2_score": r2},
     open("output/results.json", "w"), indent=4
 )
